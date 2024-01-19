8b14d8837b7819f22662dba848c62684b285625b
Vic Kuo <eun.hru@gmail.com>
2024-01-19 23:08:15 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <MainLayout>
+    <div
+      id="IndexPage"
+      class="w-full overflow-auto"
+    >
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div
+          id="Posts"
+          class="px-4 max-w-[600px] mx-auto"
+        >
+          <div
+            v-if="isPosts"
+            v-for="post in posts"
+            :key="post"
+          >
+            <Post
+              :post="post"
+              @isDeleted="posts = []"
+            />
+            <Post
+              :post="post"
+              @isDeleted="posts = []"
+            />
+            <Post
+              :post="post"
+              @isDeleted="posts = []"
+            />
+            <Post
+              :post="post"
+              @isDeleted="posts = []"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import Post from '~/components/Post.vue';
+import MainLayout from '~/layouts/MainLayout.vue';
+
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+
+let posts = ref([]);
+let isPosts = ref(true);
+let isLoading = ref(false);
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name: 'Vic Dev',
+      image: 'https://placehold.co/100',
+      text: 'This is the title',
+      picture: 'https://placehold.co/500',
+    },
+  ];
+});
+</script>
+
+<style lang="scss" scoped></style>
